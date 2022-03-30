e080e159a05cc8a81a8e4c5031cc179a29a019cf
Ste7an <51531682+ste7anste7an@users.noreply.github.com>
2022-03-30 17:11:27 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'UartRemote'
+project = 'SerialTalk'
 copyright = '2022, Antons Mindstorms Hacks'
 author = 'antonvh'
 
