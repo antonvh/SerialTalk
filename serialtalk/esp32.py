f351fca4d50d84bebdc43ee30d29274a9a664383
Anton Vanhoucke <antonvh@gmail.com>
2023-01-04 18:57:51 +0100

    removed unsupported repl

--- serialtalk/esp32.py
+++ serialtalk/esp32.py
@@ -2,22 +2,22 @@ from machine import UART
 from uos import dupterm
 
 class Esp32UART():
-    
-    DUPTERM_N = 1
 
     def __init__(self, port=1, baudrate=115200, rx_pin=18,tx_pin=19, **kwargs):
         self.rx_pin = rx_pin
         self.tx_pin = tx_pin
         self.port = port
         self.baudrate = baudrate
+        self.uart = UART(self.port, baudrate=self.baudrate,timeout=1, rx=self.rx_pin,tx=self.tx_pin)
         self.disable_repl()
 
     def disable_repl(self):
-        dupterm(None, self.DUPTERM_N)
-        self.uart = UART(self.port, baudrate=self.baudrate,timeout=1, rx=self.rx_pin,tx=self.tx_pin)
+        # Not supported on ESP32
+        pass
         
     def enable_repl(self):
-        dupterm(UART(self.port, baudrate=self.baudrate), self.DUPTERM_N)
+        # Not supported on ESP32
+        pass
 
     def any(self):
         return self.uart.any()
