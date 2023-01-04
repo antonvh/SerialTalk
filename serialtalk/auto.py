22b7bb878aa7a74244375b42fd6bbe3290378fce
Anton Vanhoucke <antonvh@gmail.com>
2023-01-04 14:05:17 +0100

    Updated openmv platform detection

--- serialtalk/auto.py
+++ serialtalk/auto.py
@@ -5,22 +5,22 @@ class SerialTalk(SerialTalk):
     def __init__(self, **kwargs):
         _platform = sys.platform
 
-        if _platform=='esp8266':
+        if _platform == 'esp8266':
             from .esp8266 import Esp8266UART
             super().__init__(Esp8266UART(**kwargs), **kwargs)
-        elif _platform=='esp32':
+        elif _platform == 'esp32':
             from .esp32 import Esp32UART
             super().__init__(Esp32UART(**kwargs), **kwargs)
         elif sys.implementation.name == 'pybricks-micropython': # Pybricks on EV3
             from .pybricks import PBSerial
             super().__init__(PBSerial(**kwargs), **kwargs)
-        elif _platform=='LEGO Learning System Hub':
+        elif _platform == 'LEGO Learning System Hub':
             from .mshub import MSHubSerial
             super().__init__(MSHubSerial(**kwargs), **kwargs)
-        elif _platform in ['OpenMV4P-H7', 'OpenMV3-M7', 'OpenMV4-H7']:
+        elif 'OpenMV' in _platform:
             from .openmv import OpenMvSerial
             super().__init__(OpenMvSerial(**kwargs), **kwargs)
-        elif _platform=='MaixPy':
+        elif _platform == 'MaixPy':
             from .k210 import K210Serial
             super().__init__(K210Serial(**kwargs), **kwargs)
         elif sys.implementation.name == 'cpython': # Regular python3
