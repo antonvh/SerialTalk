ad9451b30dca0b1163953b101fb5b941038f1902
Anton Vanhoucke <antonvh@gmail.com>
2023-03-06 22:34:47 +0100

    bug fix in uartremote polyfill

--- uartremote.py
+++ uartremote.py
@@ -1,7 +1,7 @@
 # Backward compatibility with UartRemote scripts
 
-from serialtalk.auto import SerialTalk
+from .serialtalk.auto import SerialTalk
 
 class UartRemote(SerialTalk):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
