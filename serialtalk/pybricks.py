e63bf40586e8f3e2059e57b96301c33e3e766890
Anton Vanhoucke <antonvh@gmail.com>
2023-08-29 22:41:12 +0200

    Avoid IOERR on timeout

--- serialtalk/pybricks.py
+++ serialtalk/pybricks.py
@@ -1,10 +1,11 @@
 from pybricks.iodevices import UARTDevice
 from pybricks.parameters import Port
 
-class PBSerial():
+
+class PBSerial:
     def __init__(self, port=None, baudrate=115200, **kwargs):
         # Baud rates of up to 230400 work. 115200 is the default for REPL.
-        self.baudrate=baudrate # store baudrate for repl init
+        self.baudrate = baudrate  # store baudrate for repl init
         if port is None:
             port = Port.S1
         self.uart = UARTDevice(port, baudrate, timeout=1)
@@ -12,10 +13,11 @@ class PBSerial():
     def any(self):
         return self.uart.waiting()
 
-    def read(self,n=1):
-        return self.uart.read(n)
+    def read(self, n=1):
+        try:
+            return self.uart.read(n)
+        except OSError:
+            return b''
 
     def write(self, data):
         self.uart.write(data)
-
-    
