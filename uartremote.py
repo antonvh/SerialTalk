2fd44bcbfa74f4073f2d6b2ddcda661a81b646bc
Anton Vanhoucke <antonvh@gmail.com>
2024-06-26 14:51:25 +0200

    allow for abs and rel import

--- uartremote.py
+++ uartremote.py
@@ -1,6 +1,8 @@
 # Backward compatibility with UartRemote scripts
-
-from .serialtalk.auto import SerialTalk
+try:
+    from .serialtalk.auto import SerialTalk
+except ImportError:
+    from serialtalk.auto import SerialTalk
 
 class UartRemote(SerialTalk):
     def __init__(self, *args, **kwargs):
