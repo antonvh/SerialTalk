e2a31c07b51697b2c0f24236cd208b14f24a3f94
Anton Vanhoucke <antonvh@gmail.com>
2024-06-26 14:51:47 +0200

    simplified esp32uart

--- serialtalk/esp32.py
+++ serialtalk/esp32.py
@@ -1,15 +1,9 @@
 from machine import UART
-from uos import dupterm
 
 class Esp32UART():
 
     def __init__(self, port=1, baudrate=115200, rx_pin=18,tx_pin=19, **kwargs):
-        self.rx_pin = rx_pin
-        self.tx_pin = tx_pin
-        self.port = port
-        self.baudrate = baudrate
-        self.uart = UART(self.port, baudrate=self.baudrate,timeout=1, rx=self.rx_pin,tx=self.tx_pin)
-        self.disable_repl()
+        self.uart = UART(port, baudrate=baudrate,timeout=1, rx=rx_pin,tx=tx_pin)
 
     def disable_repl(self):
         # Not supported on ESP32
